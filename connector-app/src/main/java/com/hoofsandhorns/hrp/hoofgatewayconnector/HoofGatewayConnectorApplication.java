d55f92a8bf735a9b3f61844a585dd783e2f07262
daremor <39883885+daremor@users.noreply.github.com>
2023-03-20 16:58:16 +0300

    Commit

---
+++ connector-app/src/main/java/com/hoofsandhorns/hrp/hoofgatewayconnector/HoofGatewayConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.hoofsandhorns.hrp.hoofgatewayconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoofGatewayConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoofGatewayConnectorApplication.class, args);
+	}
+
+}
---
+++ connector-app/src/test/java/com/hoofsandhorns/hrp/hoofgatewayconnector/HoofGatewayConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoofsandhorns.hrp.hoofgatewayconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoofGatewayConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
